d6f4a5816ccbc8c7a2667584e21820286afe1331
Muhammed Kara <47905966+svndevelopment@users.noreply.github.com>
2019-02-23 01:43:24 +0100

    1.0.0.0

---
+++ Extensions.cs
@@ -0,0 +1,41 @@
+﻿namespace SVN.Math
+{
+    public static class Extensions
+    {
+        public static int RoundToInt(this float param)
+        {
+            return (int)System.Math.Round(param);
+        }
+
+        public static int RoundToInt(this double param)
+        {
+            return (int)System.Math.Round(param);
+        }
+
+        public static int FloorToInt(this float param)
+        {
+            return (int)System.Math.Floor(param);
+        }
+
+        public static int FloorToInt(this double param)
+        {
+            return (int)System.Math.Floor(param);
+        }
+
+        public static int CeilingToInt(this float param)
+        {
+            return (int)System.Math.Ceiling(param);
+        }
+
+        public static int CeilingToInt(this double param)
+        {
+            return (int)System.Math.Ceiling(param);
+        }
+
+        public static int Modulo(this int param, int mod)
+        {
+            var result = (param % mod);
+            return (result < 0 ? result + mod : result);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SVN.Math")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SVN Development")]
+[assembly: AssemblyProduct("SVN.Math")]
+[assembly: AssemblyCopyright("Copyright © Muhammed Kara 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("00d32e34-d99c-489f-a8db-6b96a5f05035")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
