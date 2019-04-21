37e9642ed994585dd3d19c39687297fd9bf377d1
svndevelopment <47905966+svndevelopment@users.noreply.github.com>
2019-04-21 13:48:20 +0200

    1.1.0.0

---
+++ Random2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SVN.Math2
+{
+    public static class Random2
+    {
+        private static readonly Random Generator = new Random((int)DateTime.Now.Ticks);
+
+        public static int Range(int min, int max)
+        {
+            var diff = max - min;
+            var result = Random2.Generator.Next(diff + 1) + min;
+            return result;
+        }
+
+        public static float Range(float min, float max)
+        {
+            var diff = max - min;
+            var result = min + diff * Random2.Generator.NextDouble();
+            return (float)result;
+        }
+
+        public static double Range(double min, double max)
+        {
+            var diff = max - min;
+            var result = min + diff * Random2.Generator.NextDouble();
+            return result;
+        }
+    }
+}
